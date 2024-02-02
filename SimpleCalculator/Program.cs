cc43407144166e9f9d786ec1299224f0512bdf95
James Deas <132441520+JamesDeas@users.noreply.github.com>
2024-02-02 17:55:16 +0000

    Add files via upload

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Calculator");
+            Console.Write("Please enter an integer: ");
+            Int32 x = Int32.Parse(Console.ReadLine());
+            Console.Write("Please enter a second integer: ");
+            Int32 y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
+
+            Console.WriteLine($"{x} + {y} = {x + y}");
+            Console.WriteLine($"{x} - {y} = {x - y}");
+            Console.WriteLine($"{x} * {y} = {x * y}");
+            Console.WriteLine($"{x} / {y} = {x / y}");
+
+            Console.WriteLine($"{x} < {y} ? {x < y}");
+            Console.WriteLine($"{x} <= {y} ? {x <= y}");
+            Console.WriteLine($"{x} == {y} ? {x == y}");
+            Console.WriteLine($"{x} >= {y} ? {x >= y}");
+            Console.WriteLine($"{x} > {y} ? {x > y}");
+            Console.WriteLine($"{x} != {y} ? {x != y}");
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
